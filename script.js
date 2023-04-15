2b2dc0dca827f7cb44f99ab3002a2436ea2779fc
npcmariapaz <130719580+npcmariapaz@users.noreply.github.com>
2023-04-15 16:24:43 -0400

    antes de insertar la tabla en el script

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-$(document).ready(function(){
-        const url = 'https://digimon-api.vercel.app/api/digimon/'
-        fetch(url)
-        .then(response=> response.json())
+$(document).ready(function () {
+    const url = 'https://digimon-api.vercel.app/api/digimon/'
+    fetch(url)
+        .then(response => response.json())
         .then(json => {
             var select = document.querySelector('#opcion');
             json.forEach(option => {
                 select.appendChild(new Option(option.name, option.name));
             var listado = document.getElementById('listados');
-				json.forEach(option => {
-					var li = document.createElement('li');
-					li.appendChild(document.createTextNode(option.name));
-					listado.appendChild(li);
-                })
-    })
+            json.forEach(option => {
+                    var li = document.createElement('li');
+                    li.appendChild(document.createTextNode(option.name));
+                    listado.appendChild(li);
+                    })
+            })
 
-        }) })
+        })
+})
