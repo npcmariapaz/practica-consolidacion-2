ccb8b56882354fbd6bdd9a9097963e44bd0c4f7d
npcmariapaz <130719580+npcmariapaz@users.noreply.github.com>
2023-04-15 15:41:35 -0400

    llamado a selectores funcionando con boton de inicio

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 $(document).ready(function(){
-    document.addEventListener('DOMContentLoaded', function() {
-        fetch('https://digimon-api.vercel.app//api/digimon/name/:name')
-        .then(response => response.json())
-        .then(data => {
-            var select = document.getElementById('opcion');
-            data.forEach(option => {
-                select.appendChild(new Option(option.text, option.value));
+    $('#inicio').click(function(){
+        const url = 'https://digimon-api.vercel.app/api/digimon/'
+        fetch(url)
+        .then(response=> response.json())
+        .then(json => {
+            var select = document.querySelector('#opcion');
+            json.forEach(option => {
+                select.appendChild(new Option(option.name, option.name));
             });
         })
         .catch(error => {
@@ -14,3 +15,4 @@ $(document).ready(function(){
         });
     });
 })
+
